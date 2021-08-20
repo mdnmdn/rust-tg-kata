27b31b6362550c9a02aa5e811bbea2783035572f
Marco De Nittis <marco.denittis@gmail.com>
2021-08-21 00:21:05 +0200

    add more edge cases

--- tests/test.rs
+++ tests/test.rs
@@ -31,4 +31,24 @@ mod tests {
         let total = calculate_best_deal(max_amount, &keyboard_prices, &usb_prices);
         assert_eq!(8, total);
     }
+
+    #[test]
+    fn no_keyboard_usb() {
+        let keyboard_prices = [];
+        let usb_prices = [];
+        let max_amount = 10;
+
+        let total = calculate_best_deal(max_amount, &keyboard_prices, &usb_prices);
+        assert_eq!(0, total);
+    }
+
+    #[test]
+    fn no_amount() {
+        let keyboard_prices = [5, 1, 11, 99];
+        let usb_prices = [5, 2, 8];
+        let max_amount = 0;
+
+        let total = calculate_best_deal(max_amount, &keyboard_prices, &usb_prices);
+        assert_eq!(0, total);
+    }
 }
