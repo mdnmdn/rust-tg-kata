8aec7cf21a19e0c2d37f139cb9d0da35c5930b05
Marco De Nittis <marco.denittis@gmail.com>
2021-08-21 00:10:40 +0200

    kata 01 electronic shop   -    https://www.hackerrank.com/challenges/electronics-shop/problem

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use itertools::sorted;
+
+fn get_max_with_limit(items: &[u32], limit: u32) -> u32 {
+    items.iter().fold(0, |max, val: &u32| -> u32 {
+        let val = *val;
+        if (val > max) && (val < limit) {
+            val
+        } else {
+            max
+        }
+    })
+}
+
+
+// https://www.hackerrank.com/challenges/electronics-shop/problem
+pub fn calculate_best_deal(amount_owned: u32, keyboard_prices: &[u32], usb_prices: &[u32]) -> u32 {
+
+    // handle no keyboards
+    if keyboard_prices.is_empty() {
+        return get_max_with_limit(usb_prices, amount_owned);
+    }
+    // handle no usb
+    if usb_prices.is_empty() {
+        return get_max_with_limit(keyboard_prices, amount_owned);
+    }
+
+    let keyboard_prices: Vec<u32> = sorted(keyboard_prices.iter()).cloned().collect();
+    let usb_prices: Vec<u32> = sorted(usb_prices.iter()).cloned().collect();
+
+    let mut actual_max = 0;
+    let mut usb_pos = usb_prices.len() - 1;
+
+    'external_loop: for keyb_price in keyboard_prices.into_iter() {
+        loop {
+            let usb_price = usb_prices[usb_pos];
+
+            let combination_cost = usb_price + keyb_price;
+
+            if combination_cost > amount_owned {
+                break;
+            }
+            if combination_cost > actual_max {
+                actual_max = combination_cost;
+            } else {
+                if usb_pos == 0 {
+                    break 'external_loop;
+                }
+                usb_pos -= 1;
+            }
+        }
+    }
+
+    actual_max
+}
---
+++ tests/test.rs
@@ -0,0 +1,34 @@
+#[cfg(test)]
+mod tests {
+    use kata01_electronic_shop::*;
+
+    #[test]
+    fn test_simple_deal() {
+        let keyboard_prices = [3, 1];
+        let usb_prices = [5, 2, 8];
+        let max_amount = 10;
+
+        let total = calculate_best_deal(max_amount, &keyboard_prices, &usb_prices);
+        assert_eq!(9, total);
+    }
+
+    #[test]
+    fn no_usb() {
+        let keyboard_prices = [5, 2, 8];
+        let usb_prices = [];
+        let max_amount = 10;
+
+        let total = calculate_best_deal(max_amount, &keyboard_prices, &usb_prices);
+        assert_eq!(8, total);
+    }
+
+    #[test]
+    fn no_keyboard() {
+        let keyboard_prices = [];
+        let usb_prices = [5, 2, 8];
+        let max_amount = 10;
+
+        let total = calculate_best_deal(max_amount, &keyboard_prices, &usb_prices);
+        assert_eq!(8, total);
+    }
+}
