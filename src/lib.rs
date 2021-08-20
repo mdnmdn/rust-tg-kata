d9f0f46cf4995a750468ac9a4b7c9e4fc4e1d4de
Marco De Nittis <marco.denittis@gmail.com>
2021-08-21 01:01:24 +0200

    more refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,23 @@
 use itertools::sorted;
 
 
-fn get_max_with_limit(items: &[u32], limit: u32) -> u32 {
-    //alt:  *items.iter().filter(|&&i| i < limit).max().unwrap_or(&0)
-    items.iter().copied().filter(|i| i < &limit).max().unwrap_or(0)
-}
-
-//fn get_max_with_limit(items: &[u32], limit: u32) -> u32 {
-//    items.iter().fold(0, |max, val: &u32| -> u32 {
-//        let val = *val;
-//        if (val > max) && (val < limit) {
-//            val
-//        } else {
-//            max
-//        }
-//    })
-//}
-
 // https://www.hackerrank.com/challenges/electronics-shop/problem
 pub fn calculate_best_deal(amount_owned: u32, keyboard_prices: &[u32], usb_prices: &[u32]) -> u32 {
 
+    let usb_prices: Vec<u32> = sorted(usb_prices.iter().filter(|&&i| i < amount_owned)).copied().collect();
+
     // handle no keyboards
     if keyboard_prices.is_empty() {
-        return get_max_with_limit(usb_prices, amount_owned);
-    }
-    // handle no usb
-    if usb_prices.is_empty() {
-        return get_max_with_limit(keyboard_prices, amount_owned);
+        return usb_prices.into_iter().max().unwrap_or(0);
     }
 
-    let keyboard_prices: Vec<u32> = sorted(keyboard_prices.iter()).copied().collect();
-    let usb_prices: Vec<u32> = sorted(usb_prices.iter()).copied().collect();
+    let keyboard_prices: Vec<u32> = sorted(keyboard_prices.iter().filter(|&&i| i < amount_owned)).copied().collect();
+
+    // handle no usb
+    if usb_prices.is_empty() {
+        return keyboard_prices.into_iter().max().unwrap_or(0);
+    }
+
 
     let mut actual_max = 0;
     let mut usb_pos = usb_prices.len() - 1;
